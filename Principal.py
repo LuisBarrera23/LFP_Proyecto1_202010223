8ef7a32987792c62930d321a39c36d6a2a24ee44
LuisBarrera23 <78067073+LuisBarrera23@users.noreply.github.com>
2021-09-03 23:09:15 -0600

    creacion del area para dibujar los rectangulos

--- Principal.py
+++ Principal.py
@@ -1,4 +1,4 @@
-from tkinter import Button, Tk,ttk
+from tkinter import Button, Tk,ttk,Canvas
 class principal:
     
     def __init__(self):
@@ -18,6 +18,9 @@ class principal:
         b2=Button(self.ventana,text="Analizar",font=("Verdana",10),borderwidth=3,background="beige").place(x=120,y=20,height=40,width=100)
         b2=Button(self.ventana,text="Reportes",font=("Verdana",10),borderwidth=3,background="beige").place(x=220,y=20,height=40,width=100)
         b2=Button(self.ventana,text="Salir",font=("Verdana",10),borderwidth=3,background="Red").place(x=320,y=20,height=40,width=100)
+        self.canvas = Canvas(self.ventana,width=940, height=580, bg='white')
+        self.canvas.place(x=220,y=80)
+        self.canvas.create_rectangle(10, 10, 200, 200, width=3, fill='red')
         self.ventana.mainloop()
         
 def cargarArchivo():
